859fcb84a95a2b1b85d49ea6d36eb2c2544755d4
daniell-waqar <p200750@pwr.nu.edu.pk>
2021-12-05 23:02:05 +0500

    sort the array

---
+++ practice-asm/bubblesort.asm
@@ -0,0 +1,48 @@
+[org 0x0100]
+jmp start
+
+num: dw 20,15,5,10
+swap: db 0
+
+
+    bubblesort:
+        
+        dec cx
+        shl cx,1
+
+            main:
+                mov si,0
+                mov byte[swap],0
+
+                innerloop:
+                    mov ax,[ bx + si]
+                    cmp ax,[bx + si + 2]
+                    jbe noswap
+
+                        mov dx,[bx+ si + 2]
+                        mov [bx + si],dx
+                        mov [bx + si +2],ax
+                        mov byte[swap],1
+
+
+                noswap:
+                    add si,2
+                    cmp si , cx
+                    jne innerloop
+
+            cmp byte[swap],1
+            je main
+        ret
+
+
+start:
+    mov bx,num
+    mov cx,4
+
+    call bubblesort
+
+    mov ax,0x4c00
+    
+int 0x21
+
+
